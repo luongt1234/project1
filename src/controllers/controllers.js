d023d444f00b9fed33d97d0f4108511b5ed85ae4
luong <tranluong1680@gmail.com>
2025-04-06 14:00:21 +0700

    26/04/2025 cookie

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,6 +1,7 @@
 const { createUser, checkUser } = require('../services/CRUDservice');
 const notifier = require('node-notifier');
 const axios = require('axios'); // Thư viện gọi API
+const cookie = require('cookie-parser');
 // var popupS = require('popups');
 // let alert = require('alert');
 
@@ -23,7 +24,7 @@ const signUpUser = async (req, res) => {
                 message: 'Successful'
             });
             res.redirect('/login');
-            
+
         } else {
             notifier.notify({
                 title: 'Signup Error',
@@ -43,14 +44,32 @@ const loginAcc = async (req, res) => {
     try {
         const response = await axios.post('http://localhost:8000/login1', req.body);
         res.json(response.data); // Trả token về cho client
+        res.cookie('token', response.token, {
+            httpOnly: true,
+            secure: false,        // Bật true nếu dùng HTTPS
+            sameSite: 'Strict',
+            maxAge: 3600000        // 1 giờ
+        });
     } catch (error) {
         res.status(error.response?.status || 500).json({ message: error.response?.data?.message || 'Lỗi đăng nhập' });
     }
 }
 
-const homePage = (req, res) =>{
+const homePage = (req, res) => {
     console.log(req.header('Authorization'));
     res.render('homePage.ejs');
+}
+
+const refreshToken = async (req, res) => {
+    const response = await axios.post('http://localhost:8000/token', req.body);
+    if (response.token) {
+        res.cookie('token', response.token, {
+            httpOnly: true,
+            secure: false,        // Bật true nếu dùng HTTPS
+            sameSite: 'Strict',
+            maxAge: 3600000        // 1 giờ
+        });
+    }
 }
 
 module.exports = {
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const {login, signUp, signUpUser, loginAcc, homePage} = require('../controllers/controllers')
+const { login, signUp, signUpUser, loginAcc, homePage } = require('../controllers/controllers')
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const {verifyToken} = require('../middleware/auth')
+const { verifyToken } = require('../middleware/auth');
+const { refreshToken } = require('../controllers/athu');
 require('dotenv').config();
 
 router.get('/login', login);
@@ -17,6 +18,29 @@ router.get('/homePage', homePage);
 //     res.json({posts : 'my posts'});
 // });
 
+router.get("/token", refreshToken);
+const FILES_DIR = path.join(__dirname, 'filesUser');
+
+// Serve file tĩnh (cho phép download trực tiếp)
+// app.use('/files', express.static(FILES_DIR));
+
+// Trang chủ: hiển thị danh sách file
+app.get('/', verifyToken, (req, res) => {
+    const filePath = path.join(FILES_DIR, fileName);
+    fs.readdir(filePath, (err, files) => {
+        if (err) return res.status(500).send('Không thể đọc thư mục');
+
+        const fileListHtml = files.map(file => {
+            const fileUrl = `/files/${encodeURIComponent(file)}`;
+            return `<li><a href="${fileUrl}" download>${file}</a></li>`;
+        }).join('');
+
+        res.send(`
+      <h1>Chia sẻ file</h1>
+      <ul>${fileListHtml}</ul>
+    `);
+    });
+});
 
 
 module.exports = router;
